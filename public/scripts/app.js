b0e95224f6ae5f91cdc92bc7fffc5ea0a8cdae70
sophieluo <sophie@groobusiness.com>
2016-11-14 15:59:50 -0800

    added steps 1-3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,36 +4,6 @@
  * into functions and objects as needed.
  *
  */
-// var template;
-// var $albumsList;
-
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-    artistName: 'Ladyhawke',
-    name: 'Ladyhawke',
-    releaseDate: '2008, November 18',
-    genres: ['new wave', 'indie rock', 'synth pop']
-});
-sampleAlbums.push({
-    artistName: 'The Knife',
-    name: 'Silent Shout',
-    releaseDate: '2006, February 17',
-    genres: ['synth pop', 'electronica', 'experimental']
-});
-sampleAlbums.push({
-    artistName: 'Juno Reactor',
-    name: 'Shango',
-    releaseDate: '2000, October 9',
-    genres: ['electronic', 'goa trance', 'tribal house']
-});
-sampleAlbums.push({
-    artistName: 'Philip Wesley',
-    name: 'Dark Night of the Soul',
-    releaseDate: '2008, September 12',
-    genres: ['piano']
-});
-/* end of hard-coded data */
 
 
 
