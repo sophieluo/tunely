1c3fe062e3a14bcb0b2a1767e85710b0c84cbc9f
sophieluo <sophie@groobusiness.com>
2016-11-14 16:13:35 -0800

    setup database with mongoose

--- models/album.js
+++ models/album.js
@@ -1,2 +1,12 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+var AlbumSchema = new Schema({
+  name: String,
+  artistName: String,
+  releaseDate: String,
+  genres: String
+});
+
+var Album = mongoose.model('Album', AlbumSchema);
+module.exports = Album;
